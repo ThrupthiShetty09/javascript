2b8df7ae6df12f4d05c839b31b55f6e7b70d12cd
unknown <shettythrupthi27@gmail.com>
2018-07-13 16:28:25 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+function checkNumber() {
+    var num=document.getElementById("num").value;
+    var message;
+    if(num % 2==0){
+        
+        message= "number :"+num+" is Even";
+    }
+    else{
+        message= "number :"+num+" is Odd";
+    }
+   document.getElementById("res").innerHTML=message; 
+}
+function bill_calc(){
+    var units=document.getElementById("units").value;
+    var min_amount=50;
+    var total_amount=0;
+    var amount=0;
+    
+    if(units >=0 && units < 100){
+        amount=5*units;
+    }else if(units >=100 && units < 500){
+        amount=6*units;
+    }else{
+        amount=10*units;
+    }
+    total amount=min_amount+amount;
+    message="total bill amount:"+total_amount;
+    document.getElementById("res").innerHTML=message; 
+}
